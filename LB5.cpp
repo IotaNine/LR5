7088f03490b66d0287c82fb0c402e3f4a4fcbd56
IotaNine <70635434+IotaNine@users.noreply.github.com>
2021-04-03 20:36:39 +0300

    Update LB5.cpp

--- LB5.cpp
+++ LB5.cpp
@@ -43,19 +43,14 @@ int main()
     map1.insert(make_pair(-4, "ELEVEN"));
     for_each(map1.begin(), map1.end(), Proverka); cout << "\n";
     cout << "\t" << "End of 1.1" << "\n" << "\n";
-    bool IsNextStep = true;
     for (auto i = map1.begin(); i != map1.end();)
     {
-        if (!IsNextStep) 
-        {
-            IsNextStep = true;
-        }
         if (i->first == -3)
         {
            i = map1.erase(i);
            IsNextStep = false;
         }
-        if (IsNextStep)
+        else
         {
             i++;
         }
